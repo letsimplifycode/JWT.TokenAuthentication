fb93fb046d8148fa09bb7ca1ae5e13f30703c2e2
Anish Ravindran <30914502+anishravindran@users.noreply.github.com>
2020-07-07 07:25:27 +0530

    V1.0
    V1.0

---
+++ JWT.TokenAuthentication/Controllers/MonthController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JWT.TokenAuthentication.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MonthController : ControllerBase
+    {
+        public class Month
+        {
+            public string ShortName { get; set; }
+            public string FullName { get; set; }
+        }
+
+        [HttpGet,Authorize]
+        public IEnumerable<Month> Get()
+        {
+            var months = new Month[]
+            {
+                new Month(){ShortName ="Jan", FullName ="January"},
+                new Month() {ShortName="Feb", FullName= "February"}
+            };
+            return months;
+        }
+    }
+}
---
+++ JWT.TokenAuthentication/Controllers/TokenController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT.TokenAuthentication.Controllers
+{ 
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private IConfiguration _config;
+        public TokenController(IConfiguration config)
+        {
+            this._config = config;
+        }
+
+        [HttpGet]
+        public string CreateToken()
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+            var claims = new Claim[]
+            {
+            new Claim("user", "John Doe"),
+            new Claim("email", "John.Doe@email.com")
+            };
+            var token = new JwtSecurityToken(_config["Jwt:Issuer"],
+                _config["Jwt:Audience"],
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: creds,
+                claims: claims);
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
