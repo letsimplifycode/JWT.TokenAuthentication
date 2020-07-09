7342cb0a69fda217487e6183d7605f927ba12e9c
Anish Ravindran <30914502+anishravindran@users.noreply.github.com>
2020-07-09 15:36:58 +0530

    V 1.1
    Token Controller - Added Anonymous attribute

--- JWT.TokenAuthentication/Controllers/TokenController.cs
+++ JWT.TokenAuthentication/Controllers/TokenController.cs
@@ -5,6 +5,7 @@ using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -23,7 +24,7 @@ namespace JWT.TokenAuthentication.Controllers
             this._config = config;
         }
 
-        [HttpGet]
+        [HttpGet, AllowAnonymous]
         public string CreateToken()
         {
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config["Jwt:Key"]));
